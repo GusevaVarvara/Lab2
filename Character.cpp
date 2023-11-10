b46129b76c2446c9df5d5c7386f554a4c63b6fbf
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-11-11 00:04:55 +0300

    Update Character.cpp

--- Character.cpp
+++ Character.cpp
@@ -2,7 +2,6 @@
 
 Character::Character(const std::string& name, const Race& race, const Class& characterClass)
     : name(name), race(race), characterClass(characterClass) {
-    // Инициализация других атрибутов, если необходимо
 }
 
 const Race& Character::getRace() const {
