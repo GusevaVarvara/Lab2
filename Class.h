7f071278cff48173ae2f19a40e45105cbb47603e
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-11-30 00:02:10 +0300

    Update Class.h

--- Class.h
+++ Class.h
@@ -1,24 +1,16 @@
 #pragma once
 
-#include <string>
+#include "character.h"
 #include <vector>
 
-class Class {
+class Class : public Character {
 public:
-    Class(const std::string& name, const std::string& description);
+    Class(const std::string& name, const std::string& classname, const std::string& description);
 
-    void addAbility(const std::string& ability);
-    void addAttribute(const std::string& attributeName, int value);
-    void addBonus(const std::string& attributeToModify, int bonusValue);
-    void addPenalty(const std::string& attributeToModify, int penaltyValue);
     void addAvailableClasses(const Class& characterClass);
 
     const std::string& getName() const;
     const std::string& getDescription() const;
-    const std::vector<std::string>& getAbilities() const;
-    const std::vector<std::pair<std::string, int>>& getAttributes() const;
-    const std::vector<std::pair<std::string, int>>& getBonuses() const;
-    const std::vector<std::pair<std::string, int>>& getPenalties() const;
 
     static void initializeAvailableClasses();
     static const std::vector<Class>& getAvailableClasses();
@@ -26,10 +18,7 @@ public:
 
 private:
     static std::vector<Class> availableClasses;
+    std::string classname;
     std::string name;
     std::string description;
-    std::vector<std::string> abilities;
-    std::vector<std::pair<std::string, int>> attributes;
-    std::vector<std::pair<std::string, int>> bonuses;
-    std::vector<std::pair<std::string, int>> penalties;
 };
