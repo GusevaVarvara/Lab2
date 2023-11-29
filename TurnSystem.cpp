e980ec68203c67770f19eefe6add1f974ad5c034
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-11-30 02:24:38 +0300

    Update TurnSystem.cpp

--- TurnSystem.cpp
+++ TurnSystem.cpp
@@ -1,20 +1,15 @@
-#include "TurnSystem.h"
-#include <iostream>
+#include "turnsystem.h"
 
-TurnSystem::TurnSystem(const std::vector<std::string>& players)
-    : playerList(players), currentPlayer(playerList.begin()) {}
+TurnSystem::TurnSystem(const std::vector<Character>& players)
+    : players(players), currentPlayerIndex(0) {}
 
-const std::string& TurnSystem::getCurrentPlayer() const {
-    return *currentPlayer;
+const Character& TurnSystem::getCurrentPlayer() const {
+    return players[currentPlayerIndex];
 }
 
 void TurnSystem::nextTurn() {
-    ++currentPlayer;
-    if (currentPlayer == playerList.end()) {
-        currentPlayer = playerList.begin();
-    }
+    currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
 }
-
 
 
 
