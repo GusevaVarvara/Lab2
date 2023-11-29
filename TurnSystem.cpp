507c3bf1d411e0ee062d4853bbd69af8eb95137c
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-11-30 00:08:28 +0300

    Update TurnSystem.cpp

--- TurnSystem.cpp
+++ TurnSystem.cpp
@@ -11,7 +11,6 @@ const std::string& TurnSystem::getCurrentPlayer() const {
 void TurnSystem::nextTurn() {
     ++currentPlayer;
     if (currentPlayer == playerList.end()) {
-        // Если достигли конца списка игроков, вернемся к началу
         currentPlayer = playerList.begin();
     }
 }
@@ -20,44 +19,3 @@ void TurnSystem::nextTurn() {
 
 
 
-/*
-#include "TurnSystem.h"
-#include <algorithm>
-#include <iostream>
-
-// Реализация методов класса TurnSystem
-
-TurnSystem::TurnSystem() : currentPlayerIndex(0), roundCount(0) {
-    // Конструктор
-}
-
-void TurnSystem::startNewGame() {
-    // Инициализация системы ходов
-    roundCount = 0;
-    // Здесь вы можете добавить логику для установки начальных значений и подготовки к первому раунду
-}
-
-void TurnSystem::nextTurn() {
-    // Переключение хода на следующего игрока
-    currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
-    roundCount++;
-}
-
-bool TurnSystem::checkGameEnd() {
-    // Здесь вы можете добавить логику проверки завершения игры
-    // Например, проверить, достигнута ли какая-то цель или условие
-    return false;
-}
-
-void TurnSystem::sendMessage(const std::string& message) {
-    // Отправка сообщения текущему игроку
-    std::cout << "Player " << players[currentPlayerIndex] << ": " << message << std::endl;
-}
-
-void TurnSystem::notifyPlayers() {
-    // Уведомление игроков о текущем состоянии игры
-    for (const auto& player : players) {
-        std::cout << "Player " << player << ": It's your turn!" << std::endl;
-    }
-}
-*/
