8e570fc807a3027306ed51020e114a9483030081
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-11-30 00:06:36 +0300

    Update Dice.h

--- Dice.h
+++ Dice.h
@@ -1,37 +1,14 @@
 #pragma once
-#include <cstdlib>  // Для функции rand
-#include <ctime>    // Для функции time
+#include <cstdlib>  
+#include <ctime>    
 
 class Dice {
 public:
     Dice();
-
-    // Получить случайное число от 1 до 10
     int roll() const;
 
 private:
-    mutable bool seeded; // Флаг для отслеживания инициализации генератора случайных чисел
+    mutable bool seeded; 
 };
 
 
-/*
-#pragma once
-
-#include <string>
-
-class Dice {
-private:
-    int sides;       // Количество граней на кубике
-    int modifier;    // Модификатор, добавляемый к результату броска
-
-public:
-    // Конструктор
-    Dice(int numSides, int modifier);
-
-    // Метод для броска кубика
-    int roll() const;
-
-    // Метод для получения описания кубика
-    std::string getDescription() const;
-};
-*/
