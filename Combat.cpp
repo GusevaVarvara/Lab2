06d54aa02464de7ac1c6a7b1ccd69185c3a8d18c
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-12-13 21:41:41 +0300

    Update Combat.cpp

--- Combat.cpp
+++ Combat.cpp
@@ -1,44 +1,124 @@
 #include "combat.h"
 #include <iostream>
 
-Combat::Combat(std::vector<Character>& players, Enemy& enemy, Pet& pet)
-    : players(players), enemy(enemy), pet(pet), turnSystem(players) {}
+Combat::Combat(std::vector<Character>& players)
+    : players(players) {}
 
 void Combat::startCombat() {
     int round = 1;
     while (true) {
-        std::cout << "Round " << round << " begins!\n";
+        std::cout << "\nРаунд " << round << "\n\n";
 
-        for (const Character& player : players) {
-            std::cout << "Player's turn:\n";
-            int damage = dice.roll() * 5;
-            enemy.takeDamage(damage);
-            std::cout << "Player dealt " << damage << " damage to the enemy.\n";
+        std::cout << "Ход игрока:\n";
+        std::string diceRoll;
+        std::cout << "\nНажмите E чтобы бросить кубик\n";
+        std::cin >> diceRoll;
+        while (diceRoll != "E") {
+            std::cout << "\nНажмите E чтобы бросить кубик\n";           
+            std::cin >> diceRoll;
         }
-
-        std::cout << "Enemy's turn:\n";
-        int enemyDamage = enemy.getFixedDamage();
-        players[0].takeDamage(enemyDamage);
-        std::cout << "Enemy dealt " << enemyDamage << " damage to the player.\n";
-
-        std::cout << "Pet's turn:\n";
-        int petDamage = pet.getFixedDamage();
-        enemy.takeDamage(petDamage);
-        std::cout << "Pet dealt " << petDamage << " damage to the enemy.\n";
-
-        std::cout << "Player's health: " << players[0].getHealth() << "\n";
-        std::cout << "Enemy's health: " << enemy.getHealth() << "\n";
+        int roll = dice.roll();
+        std::cout << "\nВыпавшее значение: " << roll << "\n";
+        int damage = roll * 5;
+        players[1].takeDamage(damage);
+        std::cout << "\nИгрок нанёс " << damage << " урона врагу.\n";
 
         if (players[0].getHealth() <= 0) {
-            std::cout << "Player is defeated. Game over!\n";
+            std::cout << "\nGame over!\n";
+            break;
+        }
+        if (players[1].getHealth() <= 0) {
+            std::cout << "\nВраг повержен. Победа!\n";
+            rewardPlayer();
             break;
         }
 
-        if (enemy.getHealth() <= 0) {
-            std::cout << "Enemy is defeated. Victory!\n";
+        std::cout << "\nХод врага:\n";
+        int enemyDamage = players[1].getDamage();
+        players[0].takeDamage(enemyDamage);
+        std::cout << "Враг нанёс " << enemyDamage << " урона игроку.\n";
+
+        if (players[0].getHealth() <= 0) {
+            std::cout << "\nGame over!\n";
             break;
         }
+        if (players[1].getHealth() <= 0) {
+            std::cout << "\nВраг повержен. Победа!\n";
+            rewardPlayer();
+            break;
+        }
+
+        std::cout << "\nХод питомца:\n";
+        int petDamage = players[2].getDamage();
+        players[1].takeDamage(petDamage);
+        std::cout << "Питомец нанёс " << petDamage << " урона врагу.\n";
+
+        if (players[0].getHealth() <= 0) {
+            std::cout << "\nGame over!\n";
+            break;
+        }
+        if (players[1].getHealth() <= 0) {
+            std::cout << "\nВраг повержен. Победа!\n";
+            rewardPlayer();
+            break;
+        }
+
+        std::cout << "\n\n" << round << " раунд завершён\n";
+        std::cout << "Здоровье игрока: " << players[0].getHealth() << "\n";
+        std::cout << "Здоровье врага: " << players[1].getHealth() << "\n";
 
         round++;
     }
 }
+
+void Combat::rewardPlayer() {
+    if (players[1].getName() == "Arcadios") {
+        Weapon WeaponObj = Weapon::createWeapon("ChaosBlade");
+        Armor ArmorObj = Armor::createArmor("FlameplateArmor");
+        Potion PotionObj = Potion::createPotion("DemonicEssence");
+        
+        std::cout << "\nВы получили:\n";
+
+        std::cout << "\n" << WeaponObj.getName() << ": " << WeaponObj.getDescription() << "\n";
+        players[0].addItem(WeaponObj);
+
+        std::cout << "\n" << ArmorObj.getName() << ": " << ArmorObj.getDescription() << "\n";
+        players[0].addItem(ArmorObj);
+
+        std::cout << "\n" << PotionObj.getName() << ": " << PotionObj.getDescription() << "\n";
+        players[0].addItem(PotionObj);
+    }
+    else if (players[1].getName() == "Serafina") {
+        Weapon WeaponObj = Weapon::createWeapon("MoonshadowBow");
+        Armor ArmorObj = Armor::createArmor("ShadowcloakMantle");
+        Potion PotionObj = Potion::createPotion("ElvenEssence");
+
+        std::cout << "\nВы получили:\n";
+
+        std::cout << "\n" << WeaponObj.getName() << ": " << WeaponObj.getDescription() << "\n";
+        players[0].addItem(WeaponObj);
+
+        std::cout << "\n" << ArmorObj.getName() << ": " << ArmorObj.getDescription() << "\n";
+        players[0].addItem(ArmorObj);
+
+        std::cout << "\n" << PotionObj.getName() << ": " << PotionObj.getDescription() << "\n";
+        players[0].addItem(PotionObj);
+    }
+    else if (players[1].getName() == "Malgrim") {
+        Weapon WeaponObj = Weapon::createWeapon("BloodhoundDagger");
+        Armor ArmorObj = Armor::createArmor("BloodKissCloak");
+        Potion PotionObj = Potion::createPotion("VampiricEssence");
+
+        std::cout << "\nВы получили:\n";
+
+        std::cout << "\n" << WeaponObj.getName() << ": " << WeaponObj.getDescription() << "\n";
+        players[0].addItem(WeaponObj);
+
+        std::cout << "\n" << ArmorObj.getName() << ": " << ArmorObj.getDescription() << "\n";
+        players[0].addItem(ArmorObj);
+
+        std::cout << "\n" << PotionObj.getName() << ": " << PotionObj.getDescription() << "\n";
+        players[0].addItem(PotionObj);
+    }
+}
+
