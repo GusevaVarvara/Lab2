82ce3c223dba65f3a862b369f801572907d43c59
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-12-13 21:42:14 +0300

    Update Combat.h

--- Combat.h
+++ Combat.h
@@ -3,20 +3,22 @@
 #include "character.h"
 #include "enemy.h"
 #include "pet.h"
+#include "player.h"
 #include "dice.h"
-#include "turnsystem.h"
+#include "item.h"
+#include "weapon.h"
+#include "armor.h"
+#include "potion.h"
 
 class Combat {
 public:
-    Combat(std::vector<Character>& players, Enemy& enemy, Pet& pet);
+    Combat(std::vector<Character>& players);
 
     void startCombat();
+    void rewardPlayer();
 
 private:
     std::vector<Character>& players;
-    Enemy& enemy;
-    Pet& pet;
-    TurnSystem turnSystem;
     Dice dice;
 };
 
