295343e4b8de3bcd09786334bbab0ac3bacfda8a
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-12-13 21:45:44 +0300

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -5,27 +5,19 @@
 
 class Enemy : public Character {
 public:
-    Enemy(const std::string& name, const std::string& enemyname, const std::string& description, int initialEnemyHealth, int fixedDamage);
+    Enemy(const std::string& name, const std::string& description, int enemyHealth, int fixedDamage);
 
     void addAvailableEnemies(const Enemy& race);
 
-    virtual void takeDamage(int damage);
-    const std::string& getName() const;
     const std::string& getDescription() const;
     static const std::vector<Enemy>& getAvailableEnemies();
     static void initializeAvailableEnemies();
     static Enemy createEnemy(const std::string& enemyName);
 
-    int getEnemyHealth() const;
-    int getFixedDamage() const;
-    void setEnemyHealth(int newEnemyHealth);
 
 private:
-    int fixedDamage;
-    int enemyHealth;
     std::string description;
     std::string name;
-    std::string enemyname;
     static std::vector<Enemy> availableEnemies;
 };
 
