9efef4fb77edb5f92bae44f2572e5a288ab13a9e
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-12-13 21:45:10 +0300

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -2,8 +2,8 @@
 
 std::vector<Enemy> Enemy::availableEnemies;
 
-Enemy::Enemy(const std::string& name, const std::string& enemyname, const std::string& description, int initialEnemyHealth, int fixedDamage)
-    : Character(name), enemyname(enemyname), description(description), enemyHealth(initialEnemyHealth), fixedDamage(fixedDamage) {}
+Enemy::Enemy(const std::string& name, const std::string& description, int enemyHealth, int fixedDamage)
+    : Character(name, enemyHealth, fixedDamage), description(description) {}
 
 void Enemy::addAvailableEnemies(const Enemy& enemy) {
     availableEnemies.push_back(enemy);
@@ -11,16 +11,16 @@ void Enemy::addAvailableEnemies(const Enemy& enemy) {
 
 void Enemy::initializeAvailableEnemies() {
 
-    Enemy arcadios("Враг","Arcadios", "Могущественный демон с черной душой и армией мрака. Аркадиус питается страхом и отчаянием, "
-        "оставляя за собой след разрушений и исчезающих душ.", 50, 15);
+    Enemy arcadios("Arcadios", "Могущественный демон с черной душой и армией мрака. Аркадиус питается страхом и отчаянием, "
+                                      "оставляя за собой след разрушений и исчезающих душ.", 80, 25);
 
 
-    Enemy serafina("Враг","Serafina", " Обманчивая эльфийка, союзница темных сил. Использует свою мастерство иллюзий и контроля над разумом, "
-                                "чтобы манипулировать природой и созданиями, превращая их против группы персонажей.", 100, 10);
+    Enemy serafina("Serafina", "Обманчивая эльфийка, союзница темных сил. Использует свою мастерство иллюзий и контроля над разумом, "
+                                      "чтобы манипулировать природой и созданиями, превращая их против группы персонажей.", 120, 20);
 
 
-    Enemy malgrim("Враг","Malgrim", "Древний вампир с мрачными силами, возглавляющий легион нежити. Его жажда крови и желание "
-                                        "господства над живыми делают его страшным противником.", 150, 10);
+    Enemy malgrim("Malgrim", "Древний вампир с мрачными силами, возглавляющий легион нежити. Его жажда крови и желание "
+                                    "господства над живыми делают его страшным противником.", 150, 20);
 
     availableEnemies.push_back(arcadios);
     availableEnemies.push_back(serafina);
@@ -34,32 +34,11 @@ const std::vector<Enemy>& Enemy::getAvailableEnemies() {
     return availableEnemies;
 }
 
-const std::string& Enemy::getName() const {
-    return enemyname;
-}
 
 const std::string& Enemy::getDescription() const {
     return description;
 }
 
-int Enemy::getEnemyHealth() const {
-    return enemyHealth;
-}
-
-void Enemy::takeDamage(int damage) {
-    enemyHealth -= damage;
-    if (enemyHealth < 0) {
-        enemyHealth = 0;
-    }
-}
-
-void Enemy::setEnemyHealth(int newEnemyHealth) {
-    enemyHealth = newEnemyHealth;
-}
-
-int Enemy::getFixedDamage() const {
-    return fixedDamage;
-}
 
 Enemy Enemy::createEnemy(const std::string& enemyName) {
     const auto& enemies = Enemy::getAvailableEnemies();
@@ -69,5 +48,5 @@ Enemy Enemy::createEnemy(const std::string& enemyName) {
             return enemy;
         }
     }
-    return Enemy("?","Недоступный враг", "Описание отсутствует", 0, 0);
+    return Enemy("?", "Описание отсутствует", 0, 0);
 }
